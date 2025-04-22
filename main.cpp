83ebc7aa5218391e91acb370388339a8b4728625
VishalSoni2005 <vsoni0882@gmail.com>
2025-04-22 23:49:59 +0530

    workout 8

--- main.cpp
+++ main.cpp
@@ -1,256 +1,206 @@
-#include <GL/glut.h>
 #include <iostream>
-#include <math.h>
-#include <algorithm>
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 using namespace std;
 
-void Simpleline(int x1, int y1, int x2, int y2)
+int xmin = 50, ymin = 50, xmax = 400, ymax = 400;
+float x01, x02, y01, y02;
+int pt = 0; // Point counter
+
+void displayPoint(int x, int y)
 {
-
-  int slope;
-  if (x2 - x1 == 0)
-    slope = (y2 - y1);
-  else
-    slope = (y2 - y1) / (x2 - x1);
-
-  if (abs(slope) < 1)
-  {
-
-    if (x1 > x2)
-    {
-      swap(x1, x2);
-      swap(y1, y2);
-    }
-
-    int dx = abs(x2 - x1);
-    int dy = abs(y2 - y1);
-    // calculate p
-    int p = 2 * dy - dx;
-    int x = x1;
-    int y = y1;
+    glPointSize(2);
     glBegin(GL_POINTS);
-    while (x <= x2)
-    {
-      glVertex2f(x, y);
-      x++;
-
-      if (p >= 0)
-      {
-        if (slope < 1)
-          y++;
-        else
-          y--;
-
-        p = p + 2 * dy - 2 * dx;
-      }
-      else
-      {
-        y = y;
-        p = p + 2 * dy;
-      }
-    }
-  }
-
-  if ((abs(slope)) >= 1)
-  {
-
-    if (y1 > y2)
-    {
-      swap(x1, x2);
-      swap(y1, y2);
-    }
-
-    int dx = abs(x2 - x1);
-    int dy = abs(y2 - y1);
-
-    // calculating p
-    int p = 2 * dy - dx;
-
-    int x = x1;
-    int y = y1;
-
-    glBegin(GL_POINTS);
-    while (y <= y2)
-    {
-      glVertex2i(x, y);
-      y++;
-
-      if (p >= 0)
-      {
-        if (slope >= 1)
-          x++;
-        else
-          x--;
-
-        p = p + 2 * dx - 2 * dy;
-      }
-      else
-      {
-        x = x;
-        p = p + 2 * dx;
-      }
-    }
+    glVertex2i(x, y);
     glEnd();
-  }
-  glFlush();
 }
 
-void original(int x1, int y1, int x2, int y2)
+void SimpleLine(float x1, float y1, float x2, float y2)
 {
-  int M, p, dx, dy, x, y, t;
+    float step;
+    float dx = x2 - x1;
+    float dy = y2 - y1;
 
-  if ((x2 - x1) == 0)
-    M = (y2 - y1);
-
-  else
-    M = (y2 - y1) / (x2 - x1);
-
-  if (abs(M) < 1)
-  { // first condition of slope
-    if (x1 > x2)
+    if (abs(dx) >= abs(dy))
     {
-      swap(x1, x2);
-      swap(y1, y2);
+        step = abs(dx);
     }
-    dx = abs(x2 - x1);
-    dy = abs(y2 - y1);
-
-    p = 2 * dy - dx;
-
-    x = x1;
-    y = y1;
-
-    glBegin(GL_POINTS);
-
-    while (x <= x2)
+    else
     {
-      glVertex2i(x, y);
-      x = x + 1;
-
-      if (p >= 0)
-      {
-        if (M < 1)
-          y = y + 1;
-        else
-          y = y - 1;
-        p = p + 2 * dy - 2 * dx;
-      }
-      else
-      {
-        y = y;
-        p = p + 2 * dy;
-      }
+        step = abs(dy);
     }
-    glEnd();
-  }
 
-  if (abs(M) >= 1) // second condition of slope
-  {
-    if (y1 > y2) // common on both
+    float Xinc = dx / (float)step;
+    float Yinc = dy / (float)step;
+    float x = x1;
+    float y = y1;
+
+    for (int i = 0; i <= step; i++)
     {
-      swap(x1, x2);
-      swap(y1, y2);
+        displayPoint(x, y);
+        x = x + Xinc;
+        y = y + Yinc;
     }
-    dx = abs(x2 - x1);
-    dy = abs(y2 - y1);
+}
 
-    p = 2 * dx - dy;
+const int L = 1, R = 2, B = 4, T = 8; // Corrected bit values
 
-    x = x1;
-    y = y1;
+int calCode(double x, double y)
+{
+    int code = 0;
+    if (x < xmin)
+        code |= L;
+    if (x > xmax)
+        code |= R;
+    if (y < ymin)
+        code |= B;
+    if (y > ymax)
+        code |= T;
+    return code;
+}
 
-    glBegin(GL_POINTS);
+void LineClip(double X1, double Y1, double X2, double Y2)
+{
+    unsigned int outcode1, outcode2;
+    int accept = 0, done = 0;
+    float M = (X2 - X1 == 0) ? 0 : (float)(Y2 - Y1) / (X2 - X1); // Handle vertical lines
 
-    while (y <= y2) // ploting from y when m >= 1
+    outcode1 = calCode(X1, Y1);
+    outcode2 = calCode(X2, Y2);
 
+    do
     {
-      glVertex2f(x, y);
-      y = y + 1;
-
-      if (p >= 0)
-      {
-        if (M >= 1)
+        if ((outcode1 | outcode2) == 0)
         {
-          x = x + 1;
+            accept = 1;
+            done = 1;
+        }
+        else if ((outcode1 & outcode2) != 0)
+        {
+            done = 1;
         }
         else
         {
-          x = x - 1;
+            int outcodeOut = (outcode1 != 0) ? outcode1 : outcode2;
+            double x = 0, y = 0;
+
+            if (outcodeOut & T)
+            {
+                y = ymax;
+                x = X1 + (ymax - Y1) / M;
+            }
+            else if (outcodeOut & B)
+            {
+                y = ymin;
+                x = X1 + (ymin - Y1) / M;
+            }
+            else if (outcodeOut & L)
+            {
+                x = xmin;
+                y = Y1 + M * (xmin - X1);
+            }
+            else if (outcodeOut & R)
+            {
+                x = xmax;
+                y = Y1 + M * (xmax - X1);
+            }
+
+            if (outcodeOut == outcode1)
+            {
+                X1 = x;
+                Y1 = y;
+                outcode1 = calCode(X1, Y1);
+            }
+            else
+            {
+                X2 = x;
+                Y2 = y;
+                outcode2 = calCode(X2, Y2);
+            }
         }
-        p = p + 2 * dx - 2 * dy;
-      }
-      else
-      {
-        x = x;
-        p = p + 2 * dx;
-      }
-    }
-    glEnd();
-  }
+    } while (done == 0);
 
-  glFlush();
-}
-
-
-void drawChessboard(int boardSize)
-{
-  int squareSize = 600 / boardSize;
-  bool isWhite = true;
-
-  for (int i = 0; i <= boardSize; i++)
-  {
-    Simpleline(0, i * squareSize, boardSize * squareSize, i * squareSize);
-    Simpleline(i * squareSize, 0, i * squareSize, boardSize * squareSize);
-  }
-
-  for (int r = 0; r < boardSize; r++)
-  {
-    for (int c = 0; c < boardSize; c++)
+    if (accept)
     {
-      if ((r + c) % 2 == 0)
-      {
-        glColor3f(1.0f, 1.0f, 1.0f);
-      }
-      else
-      {
-        glColor3f(0.0f, 0.0f, 0.0f);
-      }
+        glClearColor(1.0, 1.0, 1.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glColor3f(1, 0, 0);
 
-      glBegin(GL_POLYGON);
-      glVertex2i(c * squareSize, r * squareSize);
-      glVertex2i((c + 1) * squareSize, r * squareSize);
-      glVertex2i((c + 1) * squareSize, (r + 1) * squareSize);
-      glVertex2i(c * squareSize, (r + 1) * squareSize);
-      glEnd();
+        SimpleLine(xmin, ymin, xmax, ymin);
+        SimpleLine(xmax, ymin, xmax, ymax);
+        SimpleLine(xmax, ymax, xmin, ymax);
+        SimpleLine(xmin, ymax, xmin, ymin);
+
+        glColor3f(0, 0, 1);
+        SimpleLine(X1, Y1, X2, Y2);
     }
-  }
+    glFlush();
 }
 
-void initOpenGl()
+void keyboard(unsigned char key, int x, int y)
 {
-  glClearColor(0.7, 0.7, 0.7, 1.0);
-  gluOrtho2D(0, 600, 0, 600);
-  glPointSize(1.0);
+    if (key == 'c')
+    {
+        LineClip(x01, y01, x02, y02);
+    }
 }
 
-void display()
+void myMouse(int button, int state, int x, int y)
 {
-  glClear(GL_COLOR_BUFFER_BIT);
-  drawChessboard(8);
-  glFlush();
-  glutSwapBuffers();
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
+        if (pt == 0)
+        {
+            x01 = x;
+            y01 = glutGet(GLUT_WINDOW_HEIGHT) - y;
+            pt++;
+        }
+        else
+        {
+            x02 = x;
+            y02 = glutGet(GLUT_WINDOW_HEIGHT) - y;
+            glColor3f(0, 1, 0);
+            SimpleLine(x01, y01, x02, y02);
+            pt = 0;
+        }
+    }
+    else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+    {
+        pt = 0;
+        glutKeyboardFunc(keyboard);
+    }
+    glFlush();
 }
+
+void initialize(void)
+{
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    gluOrtho2D(0, 600, 0, 600);
+}
+
+void primitives(void)
+{
+    glColor3f(1, 0, 0);
+    SimpleLine(xmin, ymin, xmax, ymin);
+    SimpleLine(xmax, ymin, xmax, ymax);
+    SimpleLine(xmax, ymax, xmin, ymax);
+    SimpleLine(xmin, ymax, xmin, ymin);
+    glFlush();
+    glutMouseFunc(myMouse);
+}
+
 int main(int argc, char **argv)
 {
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_SINGLE);
-  glutInitWindowPosition(0, 0);
-  glutInitWindowSize(600, 600);
-  glutCreateWindow("Practical_3 SIB64");
-
-  initOpenGl();
-  glutDisplayFunc(display);
-  glutMainLoop();
-  return 0;
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Added RGB
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(600, 600);
+    glutCreateWindow("SIB64");
+    initialize();
+    glutDisplayFunc(primitives);
+    glutMainLoop();
+    return 0;
 }
